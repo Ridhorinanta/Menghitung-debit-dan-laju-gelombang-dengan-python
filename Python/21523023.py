4e36bc963e20812282dbfd2d4677423257bd0c28
Ridhorinanta <93390180+Ridhorinanta@users.noreply.github.com>
2021-11-25 10:32:32 +0700

    Add files via upload

---
+++ Python/21523023.py
@@ -0,0 +1,34 @@
+#Azmi Ridho Rinanta
+#21523023
+
+#Nama
+A=("Azmi Ridho Rinanta")
+print(A)
+
+#NIM
+B=("21523023")
+print("NIM",B)
+
+print("")
+
+C=("Ini adalah rumus menghitung debit")
+print(C)
+
+D=int(input("masukkan kecepatan (v) : "))
+E=int(input("masukkan Frekuensi (f) : "))
+
+F=(D/E)
+
+print("Debit adalah (v : f) = ", F)
+
+print("")
+
+G=("Ini adalah rumus menghitung Laju Gelombang")
+print(G)
+
+H=int(input("masukkan Frekuensi (f) : "))
+I=int(input("masukkan Debit (Lambda) : "))
+
+J=(H*I)
+
+print("Laju Gelombang adalah (F * Lambda) = ", J,'m/s')
